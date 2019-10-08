684131d15262ee6964e5ba423d2767f64e1060b2
Vladimir Shefer <quameu@gmail.com>
2019-10-09 02:07:52 +0300

    Girst discord bot

--- src/main/java/com/github/vladimirshefer/gamebot/GamebotApplication.kt
+++ src/main/java/com/github/vladimirshefer/gamebot/GamebotApplication.kt
@@ -1,7 +1,10 @@
-package com.hithub.vladimirshefer.gamebot
+package com.github.vladimirshefer.gamebot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
+@JvmField
+val TOKEN = "NjMxMjUyNjY3NDQ4MTY0Mzgy.XZ0KDA.7nN58ZVwF__5_o4_Eodg1tOKW1Q"
 
 @SpringBootApplication
 class GamebotApplication
--- src/test/kotlin/com/github/vladimirshefer/gamebot/GamebotApplicationTests.kt
+++ src/test/kotlin/com/github/vladimirshefer/gamebot/GamebotApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.hithub.vladimirshefer.gamebot
+package com.github.vladimirshefer.gamebot
 
 import org.junit.Test
 import org.junit.runner.RunWith
